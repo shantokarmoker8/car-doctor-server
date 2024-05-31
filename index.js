2dad91e2342e89d0f4f05927854c8d743de16ca3
shantokarmoker8 <shantokarmoker8@gmail.com>
2024-05-31 21:35:45 +0600

    add book service

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 const express = require("express");
@@ -30,6 +30,16 @@ async function run() {
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1 },
+      };
+      const result = await serviceCollection.findOne(query, options);
       res.send(result);
     });
     // Send a ping to confirm a successful connection
